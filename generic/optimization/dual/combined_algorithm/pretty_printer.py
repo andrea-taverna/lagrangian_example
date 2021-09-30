e54aac0f7b13f782572afada3dc95d8a18248187
Andrea Taverna <andrea.taverna@outlook.com>
2021-09-30 17:46:07 +0200

    Improved spacing in log lines for lagrangian. Added schemes.

--- generic/optimization/dual/combined_algorithm/pretty_printer.py
+++ generic/optimization/dual/combined_algorithm/pretty_printer.py
@@ -5,15 +5,15 @@ from generic.optimization.dual.combined_algorithm.kpi_collector import KpiCollec
 columns_format = collections.OrderedDict(
     [
         ("iteration", ("It.", ">4d", ">4")),
-        ("dual_algorithm", ("Dual Algorithm", ">18", ">18")),
-        ("dual_bound", ("Current Dual", ">15.5g", ">15")),
+        ("dual_algorithm", ("Dual Algorithm", ">14", ">14")),
+        ("dual_bound", ("Current Dual", ">13.5g", ">13")),
         # ("primal_bound", ("Current Primal", ">15.5g", ">15")),
-        ("master_bound", ("Current Master", ">18.5g", ">18")),
-        ("best_dual_bound", ("Best Dual", ">15.5g", ">15")),
-        ("best_primal_bound", ("Best Primal", ">15.5g", ">15")),
-        ("best_master_bound", ("Best Master", ">18.5g", ">18")),
-        ("optimality_gap", ("Optimality Gap%", ">15.2%", ">15")),
-        ("cutting_plane_gap", ("CP Gap%", ">15.2%", ">15")),
+        ("master_bound", ("Current Master", ">16.5g", ">16")),
+        ("best_dual_bound", ("Best Dual", ">13.5g", ">13")),
+        ("best_primal_bound", ("Best Primal", ">13.5g", ">13")),
+        ("best_master_bound", ("Best Master", ">14.5g", ">14")),
+        ("optimality_gap", ("Optimality Gap%", ">16.2%", ">16")),
+        ("cutting_plane_gap", ("CP Gap%", ">10.2%", ">10")),
     ]
 )
 
